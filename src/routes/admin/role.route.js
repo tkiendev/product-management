7e85e16445905c796dbb7aa7a3ecabd721aec752
Kien Tran <kien@example.com>
2025-08-27 17:56:07 +0700

    Sửa lại tên file và tên biến cho đúng

---
+++ src/routes/admin/role.route.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const route = express.Router();
+
+const roleController = require('../../controllers/admin/role.controller.js');
+
+route.get('/', roleController.index);
+
+route.get('/create', roleController.create);
+route.post('/create', roleController.actionCreate);
+
+route.get('/edit/:id', roleController.edit);
+route.patch('/edit/:id', roleController.actionEdit);
+
+route.get('/detail/:id', roleController.detail);
+
+route.delete('/change-delete/:id', roleController.delete);
+
+module.exports = route;
